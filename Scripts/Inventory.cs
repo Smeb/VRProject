abd55f5ad0bfb9f95c2f9e60c86bdb6f01071438
Smeb <bryves@gmail.com>
2017-11-01 23:43:25 +0000

    Adds basic project skeleton to version control

---
+++ Scripts/Inventory.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        new Dictionary<int, int>();
+	}
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
