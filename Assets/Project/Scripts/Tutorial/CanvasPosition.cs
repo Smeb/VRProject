92e2f877e7bdbafee7178afa18988cf085c7cc33
vickyd885 <vickyd885@gmail.com>
2017-11-23 17:08:55 +0000

    add some working interaction code

---
+++ Assets/Project/Scripts/Tutorial/CanvasPosition.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasPosition : MonoBehaviour {
+
+	public MeshFilter host;
+	public RectTransform rectTransform;
+	GameObject dialogueBox;
+	// Use this for initialization
+	void Start () {
+		host = GameObject.Find ("Cowboy").GetComponentInChildren<MeshFilter>() as MeshFilter;
+		dialogueBox = GameObject.Find ("DialogueBox");
+		rectTransform = dialogueBox.GetComponent<RectTransform> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//this is your object that you want to have the UI element hovering over
+		//RectTransform CanvasRect=Canvas.GetComponent<RectTransform>();
+		RectTransform CanvasRect= this.GetComponent<RectTransform> ();
+
+	
+		//then you calculate the position of the UI element
+		//0,0 for the canvas is at the center of the screen, whereas WorldToViewPortPoint treats the lower left corner as 0,0. Because of this, you need to subtract the height / width of the canvas * 0.5 to get the correct position.
+		Vector2 ViewportPosition=Camera.main.WorldToViewportPoint(host.transform.position);
+		Vector2 WorldObject_ScreenPosition=new Vector2(
+			((ViewportPosition.x*CanvasRect.sizeDelta.x)-(CanvasRect.sizeDelta.x*0.5f)),
+			((ViewportPosition.y*CanvasRect.sizeDelta.y)-(CanvasRect.sizeDelta.y*0.5f)));
+
+
+		//now you can set the position of the ui element
+		rectTransform.anchoredPosition =WorldObject_ScreenPosition;
+
+		// Check to see if we need to display the box based on the camera direction
+		toggleVisibilityBasedOnAngle (); 
+	}
+
+
+	// Hide canvas if the user isn't facing the host
+	void toggleVisibilityBasedOnAngle(){
+		Vector3 heading = host.transform.position - Camera.main.transform.position; 
+
+		float angle = Vector3.Dot(heading, Camera.main.transform.forward);
+		//print (angle);
+
+		if (angle < 0.0) {
+			dialogueBox.SetActive (false);
+		} else {
+			dialogueBox.SetActive (true);
+		}
+
+	}
+}
---
+++ Assets/Project/Scripts/Tutorial/DialogueTextManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueTextManager : MonoBehaviour {
+
+
+	enum TutorialState { stepOne, stepTwo, finished };
+	private TutorialState state; // = TutorialState.stepOne; 
+	public Text dialogue; 
+	public GameObject host; 
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		state = TutorialState.stepOne;
+		dialogue = GameObject.Find("DialogueParent").GetComponentInChildren (typeof(Text)) as Text;
+		player = GameObject.Find ("FPSController");
+		host = GameObject.Find ("Cowboy");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (state) {
+
+		case TutorialState.stepOne:
+			if (Vector3.Distance (player.transform.position, host.transform.position) < 3) {
+				completeStepOne (); 
+				state++;
+			}
+			break;
+		
+	    /*
+		case TutorialState.stepTwo:
+			if (draggingItem.dragging != null && Vector3.Distance (player.transform.position, robot.transform.position) < 3) {
+				completeStepTwo ();
+				state++;
+			}
+			break;
+
+		case TutorialState.finished:
+			finished ();
+			break; 
+
+        */
+		}
+		
+
+	}
+
+	void completeStepOne(){
+		print ("Im close enough now!");
+		print (dialogue);
+		if (dialogue != null) {
+			print (dialogue.text);
+			dialogue.text = "Well done! Now pick up an item";
+		}
+
+	}
+
+
+}
